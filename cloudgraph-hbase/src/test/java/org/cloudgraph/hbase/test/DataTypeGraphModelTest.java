ccaab0686d60eec50f8163301faf0393e46738d0
scinnamond <scinnamond@gmail.com>
2017-08-04 14:55:17 -0700

    license update

--- cloudgraph-hbase/src/test/java/org/cloudgraph/hbase/test/DataTypeGraphModelTest.java
+++ cloudgraph-hbase/src/test/java/org/cloudgraph/hbase/test/DataTypeGraphModelTest.java
@@ -1,23 +1,17 @@
 /**
- *        CloudGraph Community Edition (CE) License
+ * Copyright 2017 TerraMeta Software, Inc.
  * 
- * This is a community release of CloudGraph, a dual-license suite of
- * Service Data Object (SDO) 2.1 services designed for relational and 
- * big-table style "cloud" databases, such as HBase and others. 
- * This particular copy of the software is released under the 
- * version 2 of the GNU General Public License. CloudGraph was developed by 
- * TerraMeta Software, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * Copyright (c) 2013, TerraMeta Software, Inc. All rights reserved.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  * 
- * General License information can be found below.
- * 
- * This distribution may include materials developed by third
- * parties. For license and attribution notices for these
- * materials, please refer to the documentation that accompanies
- * this distribution (see the "Licenses for Third-Party Components"
- * appendix) or view the online documentation at 
- * <http://cloudgraph.org/licenses/>. 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.cloudgraph.hbase.test;
 
