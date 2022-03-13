11ea02d03d7759ebb85ec8762d37fc8b35dca3a9
scinnamond <scinnamoond@gmail.com>
2022-03-13 16:16:16 -0700

    remove ae draft

--- cloudgraph-aerospike/src/main/java/org/cloudgraph/aerospike/ext/Del.java
+++
@@ -1,9 +0,0 @@
-package org.cloudgraph.aerospike.ext;
-
-public class Del extends RowMutation {
-
-  public Del(byte[] rowKey) {
-    super(rowKey);
-  }
-
-}
--- cloudgraph-aerospike/src/main/java/org/cloudgraph/aerospike/ext/Put.java
+++
@@ -1,9 +0,0 @@
-package org.cloudgraph.aerospike.ext;
-
-public class Put extends RowMutation {
-
-  public Put(byte[] rowKey) {
-    super(rowKey);
-  }
-
-}
